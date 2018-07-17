38fcc86ee1a570d4f50b260a3695468c96925c4f
Snehal Vartak <snehalvartak@users.noreply.github.com>
2018-07-17 12:40:50 -0500

    Update download.js

--- js/download.js
+++ js/download.js
@@ -1,7 +1,7 @@
 function downloadURI() 
 {
     var link = document.createElement("a");
-    link.download = "Madrina_resume";
-    link.href = "files/madrina_resume.pdf";
+    link.download = "Snehal_resume";
+    link.href = "files/Snehal_resume.pdf";
     link.click();
 }
